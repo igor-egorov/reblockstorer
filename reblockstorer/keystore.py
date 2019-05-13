5189b8bbc9e3b9db14a6e33d277e356d271ae5f1
Igor Egorov <igor@soramitsu.co.jp>
2019-05-13 15:17:06 +0300

    Refactor keystore
    Signed-off-by: Igor Egorov <igor@soramitsu.co.jp>

--- reblockstorer/keystore.py
+++ reblockstorer/keystore.py
@@ -1,23 +1,48 @@
 
 import os
+import string
 from pprint import pprint
 from iroha import IrohaCrypto
 
 
 class Keypair:
+    peers_saved = 0
 
-    def __init__(self, source_public, private, public, prefix):
+    def __init__(self, source_public, user, peer_address):
+        assert(user or peer_address)
         self.source_public_key = source_public
-        self.private_key = private
-        self.public_key = public
-        self.prefix = prefix
+        self.private_key = IrohaCrypto.private_key()
+        self.public_key = IrohaCrypto.derive_public_key(self.private_key)
+        self.user = user
+        self.peer_address = peer_address
 
-    def save(self, path, suffix):
-        file_path = os.path.join(path, self.prefix + suffix)
+    def __repr__(self):
+        d = {'private': self.private_key.decode('utf-8'),
+             'public': self.public_key.decode('utf-8')}
+        if self.user:
+            d['user'] = self.user
+        if self.peer_address:
+            d['peer'] = self.peer_address
+        return str(d)
+
+    def save(self, path):
+        if self.user:
+            prefix = self.user
+        else:
+            Keypair.peers_saved += 1
+            prefix = 'node{}{}-'.format(
+                Keypair.peers_saved, self.__format_peer_address(self.peer_address))
+        file_path = os.path.join(path, prefix)
         with open(file_path + '.priv', 'w') as priv:
-            priv.write(self.private_key)
+            priv.write(self.private_key.decode('utf-8'))
         with open(file_path + '.pub', 'w') as pub:
-            pub.write(self.public_key)
+            pub.write(self.public_key.decode('utf-8'))
+
+    def __format_peer_address(self, addr):
+        addr = addr.replace(':', '__')
+        valid_chars = '-_.(){}{}'.format(string.ascii_letters, string.digits)
+        addr = ''.join(c for c in addr if c in valid_chars)
+        return addr
 
 
 class Keystore:
@@ -26,21 +51,18 @@ class Keystore:
         self.path = keystore_path
         self.keys = {}
 
-    def renew_key(self, public_key, prefix='key'):
+    def renew_key(self, public_key, user=None, peer_address=None):
         if public_key in self.keys:
             return self.keys[public_key]
         else:
-            private = IrohaCrypto.private_key()
-            public = IrohaCrypto.derive_public_key(private)
-            keypair = Keypair(public_key, private, public, prefix)
+            assert(user or peer_address)
+            keypair = Keypair(public_key, user, peer_address)
             self.keys[public_key] = keypair
-            return public
+            return keypair
 
     def save_keys(self):
-        index = 0
         mapping_file = os.path.join(self.path, 'keys_mapping.txt')
         with open(mapping_file, 'wt') as mapping:
             pprint(self.keys, stream=mapping)
-        for _, keypair in self.keys:
-            keypair.save(str(index))
-            index += 1
+        for _, keypair in self.keys.items():
+            keypair.save(self.path)
