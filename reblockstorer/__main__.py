b340bfe2a249921437b5fe0d02dd11054e32f40c
Igor Egorov <igor@soramitsu.co.jp>
2019-05-13 15:18:11 +0300

    Update main
    Signed-off-by: Igor Egorov <igor@soramitsu.co.jp>

--- reblockstorer/__main__.py
+++ reblockstorer/__main__.py
@@ -1,14 +1,19 @@
 import sys
 
 from . import arguments
-from . import loader
+from .loader import BlockLoader
+from .keystore import Keystore
+from .processor import Processor
 
 
 def main():
     parser = arguments.init_parser()
     params = parser.parse_args()
     params = arguments.validate(parser, params)
-    loader.BlockLoader(params.blockstore)
+    block_loader = BlockLoader(params.blockstore)
+    keystore = Keystore(params.keydir)
+    processor = Processor(block_loader, keystore)
+    processor.process()
 
 
 if __name__ == "__main__":
